99ae2c8df16a6aadf128d560c726f38e6112694e
maixuanpham <51069707+maixuanpham@users.noreply.github.com>
2019-06-25 16:32:10 -0700

    Update Compare3.java

--- Compare3.java
+++ Compare3.java
@@ -2,12 +2,12 @@ public class Compare3
 {
 	public static CharSet largest(CharSet obj1, CharSet obj2, CharSet obj3) 
 	{
-       if (obj1.compareTo(obj2) > 0 && obj1.compareTo(obj3) > 0)
-           return obj1;
-       else if (obj2.compareTo(obj1) > 0 && obj2.compareTo(obj3) > 0)
-           return obj2;
-       else if (obj3.compareTo(obj1) > 0 && obj3.compareTo(obj2) > 0)
-           return obj3;
-       return obj3;
+	       if (obj1.compareTo(obj2) > 0 && obj1.compareTo(obj3) > 0)
+		   return obj1;
+	       else if (obj2.compareTo(obj1) > 0 && obj2.compareTo(obj3) > 0)
+		   return obj2;
+	       else if (obj3.compareTo(obj1) > 0 && obj3.compareTo(obj2) > 0)
+		   return obj3;
+	       return obj3;
 	} 
 }
